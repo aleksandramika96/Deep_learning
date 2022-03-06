e3d1aab3e78335f4b3f0cf93c47a52e7a0a74f1a
mikson <aleksandra.mika96@gmail.com>
2022-03-06 15:20:40 +0100

    add to training file two functions: plot training and validation accuracy/loss graph

--- Neural_network/neural_model.py
+++ Neural_network/neural_model.py
@@ -18,13 +18,13 @@ class SequentialModel:
         A sequential model is appropriate for a plain stack of layers where each layer
         has exactly one input tensor and one output tensor
     """
-
+    input_tensor_shape=int(config.get('Input and output tensors', 'input_tensor_shape'))
     # Define sequential model
     # for i in range(1,config.get('Sequential Model', layers)+1):
     #     exec(f'layer_{i} = {i}')
     model = keras.Sequential(
         [
-            layers.Dense(16, activation="relu", name="layer1", input_shape=(int(config.get('Input and output tensors', 'input_tensor_shape')),)), # relu - nonlinear function
+            layers.Dense(16, activation="relu", name="layer1", input_shape=(input_tensor_shape,)), # relu - nonlinear function
             layers.Dense(16, activation="relu", name="layer2"),
             layers.Dense(1, activation="sigmoid", name="layer3")
         ]
@@ -42,8 +42,8 @@ class FunctionalAPI:
     """ A functional API is a way to create models that are more flexible than the sequential API. It can handle models with non-linear topology, shared layers, and multiple inputs and outputs.
         The main idea is that a DL model is usually a directed acyclic graph (DAG) of layers. The functional API is a way to build graphs of layers.
      """
-
-    input_tensor = layers.Input(shape=(int(config.get('Input and output tensors', 'input_tensor_shape')),))
+    input_tensor_shape = int(config.get('Input and output tensors', 'input_tensor_shape'))
+    input_tensor = layers.Input(shape=(input_tensor_shape,))
     y = layers.Dense(16, activation="relu")(input_tensor)
     x = layers.Dense(16, activation="relu")(y)
     output_tensor = layers.Dense(1, activation="softmax")(x)
--- Neural_network/training.py
+++ Neural_network/training.py
@@ -1,19 +1,25 @@
-from keras import optimizers
+from tensorflow.keras import optimizers
 from keras.datasets import imdb
 import numpy as np
 import neural_model as nm
+import matplotlib.pyplot as plt
 
 
 # optimizer and loss function
 def model_optimizer(model):
+    """
+    args:
+        optimizer: rmsprop,
+        loss function: binary_crossentropy
+    """
+    return model.compile(optimizer=optimizers.RMSprop(learning_rate=0.001),
+                         loss='binary_crossentropy',  # mse
+                         metrics=['accuracy'])
 
-    return model.compile(optimizer=optimizers.RMSprop(lr=0.001),
-                  loss='mse',
-                  metrics=['accuracy'])
 
-def fit_model(model, input_tensor, target_tensor):
+def fit_model(model, input_tensor, target_tensor, x_val, y_val):
+    return model.fit(input_tensor, target_tensor, batch_size=512, epochs=20, validation_data=(x_val, y_val))
 
-    return model.fit(input_tensor, target_tensor, batch_size=128, epochs=10)
 
 def load_imbd_dataset(num_words=10000):
     """
@@ -29,6 +35,7 @@ def load_imbd_dataset(num_words=10000):
     (train_data, train_labels), (test_data, test_labels) = imdb.load_data(num_words=num_words)
     return (train_data, train_labels), (test_data, test_labels)
 
+
 def vectorize_sequences(sequences, dimension=10000):
     """ One-Hot Vectors Encoding """
 
@@ -36,6 +43,46 @@ def vectorize_sequences(sequences, dimension=10000):
     for i, sequence in enumerate(sequences):
         results[i, sequence] = 1.
     return results
+
+
+def plot_training_and_validation_loss_graph(history):
+    """ Visualization the training loss vs validation loss"""
+    history_dict = history.history
+    loss = history_dict['loss']
+    val_loss = history_dict['val_loss']
+    accuracy = history_dict['accuracy']
+    # val_accuracy = history_dict['val_accuracy']
+
+    epochs = range(1, len(accuracy)+1)
+
+    plt.plot(epochs, loss, 'bo', label='Training loss')
+    plt.plot(epochs, val_loss, 'b', label='Validation loss')
+    plt.title('Training and validation loss')
+    plt.xlabel('Epochs')
+    plt.ylabel('Loss')
+    plt.legend()
+
+    plt.show()
+
+
+def plot_training_and_validation_accuracy_graph(history):
+    """ Visualization the training accuracy vs validation accuracy"""
+    history_dict = history.history
+    # loss = history_dict['loss']
+    # val_loss = history_dict['val_loss']
+    accuracy = history_dict['accuracy']
+    val_accuracy = history_dict['val_accuracy']
+
+    epochs = range(1, len(accuracy) + 1)
+
+    plt.plot(epochs, accuracy, 'bo', label='Training accuracy')
+    plt.plot(epochs, val_accuracy, 'b', label='Validation accuracy')
+    plt.title('Training and validation accuracy')
+    plt.xlabel('Epochs')
+    plt.ylabel('Loss')
+    plt.legend()
+
+    plt.show()
 
 
 (train_data, train_labels), (test_data, test_labels) = load_imbd_dataset()
@@ -48,4 +95,22 @@ x_test = vectorize_sequences(test_data)
 y_train = np.asarray(train_labels).astype('float32')
 y_test = np.asarray(test_labels).astype('float32')
 
-model = nm.SequentialModel.model
+sm_model = nm.SequentialModel()
+model = sm_model.model
+
+# configuring model, optimizer: rmsprop, loss function: binary_crossentropy
+model_optimizer(model)
+
+# model validation, creating validation dataset
+x_val = x_train[:sm_model.input_tensor_shape]
+partial_x_train = x_train[sm_model.input_tensor_shape:]
+
+y_val = y_train[:sm_model.input_tensor_shape]
+partial_y_train = y_train[sm_model.input_tensor_shape:]
+
+history = fit_model(model, input_tensor=partial_x_train, target_tensor=partial_y_train, x_val=x_val, y_val=y_val)
+# history_dict = history.history
+# print(history_dict.keys())  # dict_keys(['loss', 'accuracy', 'val_loss', 'val_accuracy'])
+
+plot_training_and_validation_accuracy_graph(history)
+plot_training_and_validation_loss_graph(history)
