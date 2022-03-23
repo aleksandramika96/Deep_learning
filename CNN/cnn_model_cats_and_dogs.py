2e5c9b342b819d3b6d52e67d5a562a27f7c9d86a
mikson <aleksandra.mika96@gmail.com>
2022-03-23 13:53:46 +0100

    cnn model cats and dogs and update requirements libraries

--- CNN/cnn_model_cats_and_dogs.py
+++ CNN/cnn_model_cats_and_dogs.py
@@ -2,5 +2,17 @@ from keras import layers
 from keras import models
 
 model = models.Sequential()
-model.add(layers.Conv2D(32, (3,3), activation='relu',
-                        input_shape=(150,150,3)))
+model.add(layers.Conv2D(32, (3, 3), activation='relu', # 32 - głębia, 3x3 - wymiary okien, relu - funkcja aktywacji
+                        input_shape=(150, 150, 3))) # w×h×c ; input images size 150x150 ; 3 channels
+model.add(layers.MaxPooling2D((2,2)))
+model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D((2,2)))
+model.add(layers.Conv2D(128, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D((2,2)))
+model.add(layers.Conv2D(128, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D((2,2)))
+model.add(layers.Flatten())
+model.add(layers.Dense(512, activation='relu'))
+model.add(layers.Dense(1, activation='sigmoid'))
+
+model.summary()
