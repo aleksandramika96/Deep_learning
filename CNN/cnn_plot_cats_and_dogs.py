1ac5f12a3d097a2d30ebd672b5bfddc27e0e2f9b
mikson <aleksandra.mika96@gmail.com>
2022-04-05 18:02:38 +0200

    add plot script; update a requirements file

--- CNN/cnn_plot_cats_and_dogs.py
+++ CNN/cnn_plot_cats_and_dogs.py
@@ -11,16 +11,15 @@ val_loss = history.history['val_loss']
 
 epochs = range(len(acc))
 
-plt.plot(epochs, acc, 'bo', label='Training accuracy')
-plt.plot(epochs, val_acc, 'b', label='Validation accuracy')
+plt.plot(epochs, acc, 'bo', label='Training accuracy', color='orange')
+plt.plot(epochs, val_acc, 'b', label='Validation accuracy', color='lightgrey')
 plt.title('Training and validation accuracy')
 plt.legend()
-
-plt.figure()
 plt.savefig('training_validation_acc.png', dpi=100)
+plt.figure()
 
-plt.plot(epochs, loss, 'bo', label='Training loss')
-plt.plot(epochs, val_loss, 'b', label='Validation loss')
+plt.plot(epochs, loss, 'bo', label='Training loss', color='orange')
+plt.plot(epochs, val_loss, 'b', label='Validation loss', color='lightgrey')
 plt.title('Training and validation loss')
 plt.legend()
 plt.savefig('training_validation_loss.png', dpi=100)
